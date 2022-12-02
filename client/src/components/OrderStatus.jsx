18f1b907061a8d51f0b61e2ab94d570154b5a34c
harsharma2026 <harsharma2026@gmail.com>
2022-12-03 03:56:34 +0530

    Fully completed

--- client/src/components/OrderStatus.jsx
+++ client/src/components/OrderStatus.jsx
@@ -69,7 +69,7 @@ export default function OrderStatus() {
                 <div className="flex justify-between space-x-8 items-start w-full">
                   <p className="text-base xl:text-lg leading-6">
                   ${val.amount}
-                    <span className="text-red-300 line-through"> $45.00</span>
+                    
                   </p>
                   <p className="text-base xl:text-lg leading-6 text-gray-800">
                     01
@@ -203,11 +203,7 @@ export default function OrderStatus() {
                   </p>
                 </div>
               </div>
-              <div className="flex w-full justify-center items-center md:justify-start md:items-start">
-                <button className="mt-6 md:mt-0 py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base leading-4 text-gray-800">
-                  Edit Details
-                </button>
-              </div>
+              
             </div>
           </div>
         </div>
